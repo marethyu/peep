1de2aba88671458b43b7b5cd8951078b2121528c
Jimmy Y <codingexpert123@gmail.com>
2020-02-09 21:53:41 -0800

    oops, forgot to import sys

--- peep/util.py
+++ peep/util.py
@@ -1,6 +1,8 @@
 # originally this code was in peep.py but it was moved to this seperate file
 # why? see https://stackoverflow.com/questions/60034869/python-global-variable-is-none-in-another-module
 
+import sys
+
 filename = None
 
 def get_file(file):
