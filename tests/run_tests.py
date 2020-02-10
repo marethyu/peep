c158ed1ddc4cb0a9e0e80dbc7b39a4a72abe0a84
Jimmy Y <codingexpert123@gmail.com>
2020-02-09 23:43:32 -0800

    Made a utility script and edited run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,14 +4,19 @@ import sys
 from os.path import abspath, dirname, isfile, join
 from unittest.mock import patch
 
+def mock_exit():
+    pass
+
 def run(file):
-    from peep.peep import main
-    with patch.object(sys, 'argv', ["", "-i", file]):
+    with patch(sys, "argv", ["", "-i", file]), patch(sys, "exit", mock_exit):
+        from peep.peep import main
         main()
 
 if __name__ == "__main__":
     sys.path.append(dirname(dirname(abspath(__file__))))
     current_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    # use itertools
     
     v = 1
     
@@ -24,7 +29,6 @@ if __name__ == "__main__":
             no = 1
             
             for file in os.listdir(child_dir):
-                if "err" not in file:
-                    print("TEST {} (file={})".format(no, file))
-                    no += 1
-                    run(join(child_dir, file))
+                print("TEST {} (file={})".format(no, file))
+                no += 1
+                run(join(child_dir, file))
