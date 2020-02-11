df774defaf0b2cd7ce34265b2175fae905ff1765
Jimmy Y <codingexpert123@gmail.com>
2020-02-10 19:10:04 -0800

    Fixed errors

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,34 +1,30 @@
 import os
 import sys
 
-from os.path import abspath, dirname, isfile, join
+from os.path import abspath, dirname, isfile, join, realpath
 from unittest.mock import patch
 
-def mock_exit():
-    pass
-
 def run(file):
-    with patch(sys, "argv", ["", "-i", file]), patch(sys, "exit", mock_exit):
-        from peep.peep import main
-        main()
+    def exit(x):
+        raise
+    
+    with patch("sys.argv", ["", "-i", file]), patch("sys.exit", exit):
+        try:
+            from peep.peep import main
+            main()
+        except:
+            return
 
 if __name__ == "__main__":
     sys.path.append(dirname(dirname(abspath(__file__))))
-    current_dir = os.path.dirname(os.path.realpath(__file__))
+    current_dir = dirname(realpath(__file__))
     
-    # use itertools
-    
-    v = 1
-    
-    for dir in os.listdir(current_dir):
+    for v, dir in enumerate(os.listdir(current_dir)):
         child_dir = join(current_dir, dir)
         
         if not isfile(child_dir):
-            print("VERSION {} TEST".format(v))
-            v += 1
-            no = 1
+            print("VERSION {} TEST".format(v + 1))
             
-            for file in os.listdir(child_dir):
-                print("TEST {} (file={})".format(no, file))
-                no += 1
+            for no, file in enumerate(os.listdir(child_dir)):
+                print("TEST {} (file={})".format(no + 1, file))
                 run(join(child_dir, file))
